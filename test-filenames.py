15ead925391f812a74ff759804789b9ff5f80a01
Alberto Bortoni <alb.bortoni@gmail.com>
2018-03-18 16:21:14 -0400

    update

--- test-filenames.py
+++ test-filenames.py
@@ -10,7 +10,7 @@ from subprocess import PIPE, STDOUT
 import shlex
 from time import sleep
 import RPi.GPIO as GPIO
-
+from random import *
 
 GPIO.setmode(GPIO.BCM)
 butt = 13
@@ -19,23 +19,32 @@ GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print('started')
 
-
 print('hello, world!')
 test = "/home/eleven/Music/"
 song = "coldplay-cemeteriesOfLondon.mp3"
 
-player = subprocess.Popen(["mplayer", "-volume", "50", "-volstep", "2", test+song], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+dirs = os.listdir(test)
+print(dirs)
 
-time.sleep(5)
+siz=len(dirs)
+
+print(siz)
+rand = randint(1, siz)-1
+print(rand)
+print(dirs[rand])
+
+#player = subprocess.Popen(["mplayer", "-volume", "50", "-volstep", "2", test+dirs[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+#time.sleep(10)
 #player.terminate()
 
 print('again!')
 
 
-while True:
-  if(GPIO.input(butt)==0):
-    print('22')
-    player.stdin.write('p')
-    player.stdin.flush()
+#while True:
+if(GPIO.input(butt)==0):
+  print('22')
+  #player.stdin.write('p')
+  #player.stdin.flush()
 
-  time.sleep(1)
+#time.sleep(1)

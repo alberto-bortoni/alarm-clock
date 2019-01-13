de530ba6a37b476748f486a34b2dacf4bad990c8
alberto bortoni <alb.deg@gmail.com>
2019-01-13 17:23:48 -0500

    volume fns

--- alarm-eleven.py
+++ alarm-eleven.py
@@ -25,6 +25,7 @@ volume = 10
 volumeStep = 2
 maxVol = 110
 medVol = 90
+minVol = 60
 
 butt = 13
 exitFlag = 0
@@ -83,6 +84,24 @@ def butt_callback(channel):
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(butt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(butt, GPIO.BOTH, callback=butt_callback)  # add rising edge detection on a channel
+
+#################################
+#       VOLUME CONTROLS         #
+#################################
+def increase_volume():
+  global volume, player, maxVol
+  if (volume < maxVol):
+    player.stdin.write('0')
+    player.stdin.flush()
+    volume = volume + volumeStep
+
+def decrease_volume():
+  global volume, player, minVol
+  if (volume > minVol):
+    player.stdin.write('9')
+    player.stdin.flush()
+    volume = volume - volumeStep
+
 #################################
 #             MAIN              #
 #################################
@@ -99,35 +118,19 @@ exitSequence = 'sslssl' #button press sequence for shutting off alarm
 while exitFlag==0:
 
   if (time.time()-lastTime >= timeIncVol):  #increase volume
-    if(volume<maxVol):
-      player.stdin.write('0')
-      player.stdin.flush()
-      lastTime = time.time()
-      volume = volume+volumeStep
+    increase_volume()
 
-  if(GPIO.input(butt)==0):                  #start snooze/exit
-    time.sleep(0.25)
-    buttTime = time.time()
+  if(longPressWaiting or shortPressWaiting):                  #start snooze/exit
+    decrease_volume()                       #reduce noise every button
 
-    while (time.time()-buttTime < timeButt):
-
-      if(GPIO.input(butt)==0):              #count snooze, reset timer
-        buttTime = time.time()
-        snooze = snooze+1
-        for num in range(1, snoozeInc):
-          player.stdin.write('9')           #reduce noise every button
-          player.stdin.flush()
-          time.sleep(0.25)
-          volume = volume-volumeStep
-
-      if(exitSequence in buttSequence):            #exit, snoozed
-        player.terminate()
-        exitFlag=1
-        alarm = subprocess.Popen(["mplayer", "-volume", "75", "-really-quiet", "/home/eleven/Alarms/Alarm03.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        time.sleep(6)
-        alarm.terminate()
-
-    snooze = 0                              #reset counter
+    print "Checking sequence now"
+    if(exitSequence in buttSequence):            #exit, snoozed
+      print "Exit Sequence Activated"
+      player.terminate()
+      exitFlag=1
+      alarm = subprocess.Popen(["mplayer", "-volume", "75", "-really-quiet", "/home/eleven/Alarms/Alarm03.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+      time.sleep(6)
+      alarm.terminate()
 
   if (time.time()-startTime > 600):         #exit program, too long
     player.terminate()

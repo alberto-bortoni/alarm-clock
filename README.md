1cecf9c6651b69cf5905cfa0204cb8f8416d506d
alberto bortoni <alb.deg@gmail.com>
2019-03-31 19:59:28 -0400

    added shutdown request functionality

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 to install, follow the instructions on the file crontabEnt
 then run the following commands on the terminal
 
-sudo mv listen-for-shutdown.py /usr/local/bin/
+sudo cp listen-for-shutdown.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/listen-for-shutdown.py
-sudo mv listen-for-shutdown.sh /etc/init.d/
+sudo cp listen-for-shutdown.sh /etc/init.d/
 sudo chmod +x /etc/init.d/listen-for-shutdown.sh
 sudo update-rc.d listen-for-shutdown.sh defaults
 sudo /etc/init.d/listen-for-shutdown.sh start
--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
+import mpylayer
+import os
+import time
 import subprocess
+from subprocess import PIPE, STDOUT
+import shlex
+from time import sleep
+import RPi.GPIO as GPIO
+
+
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(13, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+  falling = GPIO.wait_for_edge(13, GPIO.FALLING)
+  time.sleep(0.2)
+
+  if falling is not None:
+    rising = GPIO.wait_for_edge(13, GPIO.RISING, timeout=10000)
+    if rising is None
+      print "shutdown"
+      #subprocess.call(['shutdown', '-h', 'now'], shell=False)
